f9299cf56ca27efbf7a81e584a44138a46464711
jacikk <devjacik@outlook.com>
2025-06-18 17:09:15 -0300

    added validation to kafka sync

--- connection/pub_users.go
+++ connection/pub_users.go
@@ -6,19 +6,21 @@ import (
 	"log"
 	"time"
 
+	"github.com/dev-star-company/custom-validate/validate"
 	"github.com/dev-star-company/kafka-go/topics"
 	"github.com/segmentio/kafka-go"
 )
 
 type SyncUserStruct struct {
-	ID          int64  `json:"id"`
-	RequesterID int64  `json:"requester_id"`
-	Name        *string `json:"name"`
-	Surname     *string `json:"surname"`
-	CreatedAt   *string `json:"created_at"`
-	UpdatedAt   *string `json:"updated_at"`
-	CreatedBy   *int64  `json:"created_by"`
-	UpdatedBy   *int64  `json:"updated_by"`
+	ID        int64   `json:"id"`
+	Name      *string `json:"name"`
+	Surname   *string `json:"surname"`
+	CreatedAt *string `json:"created_at"`
+	UpdatedAt *string `json:"updated_at"`
+	CreatedBy *int64  `json:"created_by"`
+	UpdatedBy *int64  `json:"updated_by"`
+	DeletedAt *string `json:"deleted_at,omitempty"`
+	DeletedBy *int64  `json:"deleted_by,omitempty"`
 }
 
 func (p Connectioner) PublishToSyncUsers(message Message[SyncUserStruct]) error {
@@ -27,8 +29,52 @@ func (p Connectioner) PublishToSyncUsers(message Message[SyncUserStruct]) error 
 		return errors.New("connection not found for topic SyncUsers")
 	}
 
-	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	if message.Action != "create" && message.Action != "update" && message.Action != "delete" {
+		return errors.New("invalid action: must be 'create', 'update', or 'delete'")
+	}
 
+	switch message.Action {
+	case "create":
+		fields := map[string]string{
+			"Id":        "required,numeric,min=1",
+			"Name":      "required,min=3",
+			"Surname":   "required,min=3",
+			"CreatedAt": "required,datetime",
+			"UpdatedAt": "required,datetime",
+			"CreatedBy": "required,numeric,min=1",
+			"UpdatedBy": "required,numeric,min=1",
+		}
+
+		if err := validate.Validate(fields, message.Payload); err != nil {
+			return err
+		}
+	case "update":
+		fields := map[string]string{
+			"Id":        "required,numeric,min=1",
+			"Name":      "optional,min=3",
+			"Surname":   "optional,min=3",
+			"UpdatedAt": "required,datetime",
+			"UpdatedBy": "required,numeric,min=1",
+			"DeletedAt": "optional,datetime",
+			"DeletedBy": "optional,numeric,min=1",
+		}
+
+		if err := validate.Validate(fields, message.Payload); err != nil {
+			return err
+		}
+	case "delete":
+		fields := map[string]string{
+			"Id":         "required,numeric,min=1",
+			"DetectedAt": "required,datetime",
+			"DetectedBy": "required,numeric,min=1",
+		}
+
+		if err := validate.Validate(fields, message.Payload); err != nil {
+			return err
+		}
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
 	userBytes, err := json.Marshal(message)
 	if err != nil {
 		log.Fatal("failed to marshal user:", err)
